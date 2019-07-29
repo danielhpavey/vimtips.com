762a75fd170411f33f783b7ac1cd1af9600f4822
Daniel <danielhpavey@gmail.com>
2019-07-29 19:39:36 +0000

    Update site title

--- gridsome.config.js
+++ gridsome.config.js
@@ -1,12 +1,12 @@
-// This is where project configuration and plugin options are located. 
+// This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here requires a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+  siteName: 'Vim Tips',
+  siteDescription: 'Just a home for my rag-tag collection of vim tips',
 
   plugins: [
     {
